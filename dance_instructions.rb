f19b884d1074df892d733de1c1a7485dca4f39fc
riverfife <riverfife@gmail.com>
2018-07-24 16:43:10 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,11 +1,19 @@
 
 # Code your methods below
 
+def assume_stance
+  
+  puts plant legs far apart, bend knees slightly and keep posture loose
+  
+def skip_step
+  
+  puts #1 
+  
 def base_footwork
   
 puts #1 lift right foot
  puts #2 return right foot to the ground 
- puts #3 finish with a small skip_step
+ puts #3 finish with a small skip_step backwards
  
  end
  
